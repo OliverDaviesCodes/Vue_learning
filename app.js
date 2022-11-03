5cdeb210bed9e3b9e6efbcc5614b8a323ce0f9db
Oliver Davies <79981028+OliverDaviesCodes@users.noreply.github.com>
2022-11-03 09:07:45 +0200

    Created initial application files and code

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+/*El is short for event listener */
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
 
+function addGoal() {
+    const enteredValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEl.appendChild(listItemEl)
+}
+
+buttonEl.addEventListener('click', addGoal);
